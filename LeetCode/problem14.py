c7bb863d8806976293b24c63f1e0f0f952bb266a
Didrik Lindberg Roest <didrikr@viken.no>
2023-09-11 13:33:28 +0200

    problem

---
+++ LeetCode/problem14.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+14. Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+"""
+
+strs = ["flower","flow","flight"]
+#strs = ["dog","racecar","car"]
+
+output = ""
+
