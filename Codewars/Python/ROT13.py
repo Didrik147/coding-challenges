ba3857c04edc4a05d8925afc115f481d6a02f3bb
Didrik Roest <didrik.roest@yahoo.com>
2024-06-06 09:39:37 +0200

    Update ROT13.py

--- Codewars/Python/ROT13.py
+++ Codewars/Python/ROT13.py
@@ -1,16 +1,25 @@
-# Work in progress (not done)
-
 import string
 
 def rot13(message):
-    alphabet = list(string.ascii_lowercase)
+    alphabet_lower = list(string.ascii_lowercase)
+    alphabet_upper = list(string.ascii_uppercase)
     
     output = ""
     
     for letter in message:
-        if letter.lower() in alphabet:
-            i = alphabet.index(letter.lower())
+        if letter in alphabet_lower:
+            i = alphabet_lower.index(letter)
+            output += alphabet_lower[(i+13)%26]
+            
+        elif letter in alphabet_upper:
+            i = alphabet_upper.index(letter)
+            output += alphabet_upper[(i+13)%26]
+            
+        else:
+            output += letter
 
+    return output
 
 print(rot13("EBG13 rknzcyr."))
 print(rot13("This is my first ROT13 excercise!"))
+print(rot13("test"))
