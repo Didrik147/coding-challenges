224d68caef7dd14d9e8a9c9c1bbca40d6b601187
Didrik147 <didrik.roest@yahoo.com>
2025-08-11 21:01:15 +0200

    LeetCode

--- LeetCode/SQL/problem570.sql
+++ LeetCode/SQL/problem570.sql
@@ -9,3 +9,9 @@ WHERE id IN
   HAVING COUNT(*) >= 5
 )
 
+-- Alternative solution
+SELECT e.name
+FROM Employee AS e
+JOIN Employee AS m ON e.id = m.managerId
+GROUP BY m.managerId
+HAVING COUNT(m.managerID) >= 5
